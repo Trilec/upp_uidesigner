f9fbe7275d31840fad066b084014f88dfdfe769a
Curtis <Trilec@users.noreply.github.com>
2026-08-20 19:16:50 +1200

    Define compact Theme Builder toolbar and matrix preview

--- UiDesigner/Theme/UiDesignerThemeGallery.h
+++ UiDesigner/Theme/UiDesignerThemeGallery.h
@@ -5,13 +5,86 @@
 #include <Ui/Ui.h>
 #include <Ui/UiColorPicker/UiColorPicker.h>
 #include <UiDesigner/Catalog/UiDesignerCatalog.h>
-#include <UiDesigner/Preview/UiDesignerPreview.h>
 #include <UiDesigner/ThemeCore/UiDesignerTheme.h>
 
 namespace Upp {
 
 UiThemeContext UiDesignerResolveThemeContext(const UiDesignerThemeSnapshot& theme);
 void UiDesignerApplyGlobalTheme(const UiDesignerThemeSnapshot& theme);
+
+class UiDesignerThemeSwatch : public Ctrl {
+public:
+    typedef UiDesignerThemeSwatch CLASSNAME;
+
+    UiDesignerThemeSwatch();
+
+    UiDesignerThemeSwatch& SetColor(Color color);
+    Color GetColor() const { return color_; }
+    UiDesignerThemeSwatch& SetActive(bool active = true);
+
+    virtual Size GetMinSize() const override { return Size(DPI(18), DPI(18)); }
+    virtual void Paint(Draw& w) override;
+    virtual void LeftDown(Point p, dword keyflags) override;
+    virtual void LeftUp(Point p, dword keyflags) override;
+    virtual void LeftDrag(Point p, dword keyflags) override;
+    virtual bool Key(dword key, int count) override;
+
+    Event<> WhenAction;
+
+private:
+    String HexText() const;
+
+    Color color_ = Color(128, 128, 128);
+    bool active_ = false;
+    bool dragging_ = false;
+};
+
+class UiDesignerThemeGallery;
+
+class UiDesignerThemeToolbar : public ParentCtrl {
+public:
+    typedef UiDesignerThemeToolbar CLASSNAME;
+
+    UiDesignerThemeToolbar();
+
+    void SetThemeDocument(UiDesignerThemeDocument *theme);
+    void SetGallery(UiDesignerThemeGallery *gallery);
+    void ApplyTheme(const UiDesignerThemeSnapshot& theme);
+    void SyncFromTheme();
+
+    virtual Size GetMinSize() const override { return Size(DPI(760), DPI(50)); }
+    virtual void Layout() override;
+    virtual void Paint(Draw& w) override;
+
+    Event<String> WhenStatus;
+
+private:
+    void SetPreviewMode(const String& mode);
+    void SetPanelRole(UiRole role);
+    void SetControlRole(UiRole role);
+    void ToggleAppearance();
+    void EditPalette(bool dark, int active_slot);
+    void UpdateModeButtons();
+
+    UiDesignerThemeDocument *theme_ = nullptr;
+    UiDesignerThemeGallery *gallery_ = nullptr;
+    bool syncing_ = false;
+    String preview_mode_ = "controls";
+    UiRole panel_role_ = UiRole::Accent;
+    UiRole control_role_ = UiRole::Accent;
+
+    UiToolButton controls_mode_;
+    UiToolButton containers_mode_;
+    UiLabel panel_role_label_;
+    UiDropdown panel_role_drop_;
+    UiLabel control_role_label_;
+    UiDropdown control_role_drop_;
+    UiToolButton appearance_;
+    UiLabel light_label_;
+    UiLabel dark_label_;
+    UiDesignerThemeSwatch light_[UI_DESIGNER_THEME_PALETTE_SIZE];
+    UiDesignerThemeSwatch dark_[UI_DESIGNER_THEME_PALETTE_SIZE];
+};
 
 class UiDesignerThemeGallery : public ParentCtrl {
 public:
@@ -21,53 +94,48 @@ public:
 
     void SetCatalog(const UiDesignerCatalog *catalog);
     void SetThemeDocument(UiDesignerThemeDocument *theme);
-    void SetFilter(const String& filter);
-    void RebuildInventory();
+    void SetPreviewMode(const String& mode);
+    void SetPanelRole(UiRole role);
+    void SetControlRole(UiRole role);
+    void RefreshTheme();
     int GetContentHeight() const { return content_height_; }
+
+    // Compatibility with the previous gallery filter API. Theme Builder now
+    // has only the two purposeful preview modes.
+    void SetFilter(const String& filter)
+    {
+        SetPreviewMode(ToLower(filter) == "containers" ? "containers" : "controls");
+    }
 
     virtual void Layout() override;
     virtual void Paint(Draw& w) override;
 
 private:
-    void BuildAuthoredControls();
+    void BuildPreviewMatrices();
+    void BuildControlSamples();
+    void BuildContainerSamples();
     void ApplyThemeStyles();
-    void SyncPaletteEditor();
-    void CommitPaletteSlot(int slot);
-    void Put(Ctrl& ctrl, int x, int y, int cx, int cy);
+    void LayoutControlSamples();
+    void LayoutContainerSamples();
 
     const UiDesignerCatalog *catalog_ = nullptr;
     UiDesignerThemeDocument *theme_ = nullptr;
-    String filter_ = "all";
-    int content_height_ = 1200;
-    bool palette_syncing_ = false;
-    bool edit_dark_palette_ = false;
+    String preview_mode_ = "controls";
+    UiRole panel_role_ = UiRole::Accent;
+    UiRole control_role_ = UiRole::Accent;
+    int content_height_ = DPI(730);
 
-    UiTitleCard title_card_;
+    UiStack preview_stack_;
+    UiGridLayout controls_matrix_;
+    UiGridLayout containers_matrix_;
+    UiBoxLayout control_columns_[3];
+    UiBoxLayout container_columns_[3];
 
-    UiGroupPanel palette_group_;
-    UiDropdown palette_mode_;
-    UiLabel palette_help_;
-    UiColorPicker palette_editor_;
-
-    UiGroupPanel control_roles_group_;
-    UiButton role_standard_;
-    UiButton role_subtle_;
-    UiButton role_accent_;
-    UiButton role_alert_;
-
-    UiGroupPanel panel_roles_group_;
-    UiPanel panel_surface_;
-    UiPanel panel_subtle_;
-    UiPanel panel_strong_;
-    UiLabel panel_surface_label_;
-    UiLabel panel_subtle_label_;
-    UiLabel panel_strong_label_;
-
-    UiGroupPanel toggle_group_;
-    UiLabel toggle_label_a_;
-    UiLabel toggle_label_b_;
-    UiToggle toggle_a_;
-    UiToggle toggle_b_;
+    // Controls view: representative families hosted on independently themed
+    // panels/group panels.
+    UiPanel controls_reference_panel_;
+    UiLabel controls_reference_label_;
+    UiButton controls_reference_button_;
 
     UiGroupPanel buttons_group_;
     UiButton button_;
@@ -75,43 +143,65 @@ private:
     UiSplitButton split_button_;
     UiBreadcrumbs breadcrumbs_;
 
+    UiGroupPanel choices_group_;
+    UiCheckBox check_;
+    UiRadioButton radio_;
+    UiToggle toggle_;
+    UiDropdown dropdown_;
+
     UiGroupPanel numbers_group_;
     UiIntEdit int_edit_;
     UiFloatEdit float_edit_;
     UiSlider slider_;
     UiProgressBar progress_;
 
-    UiGroupPanel security_group_;
-    UiMaskEdit mask_edit_;
-    UiPasswordEdit password_edit_;
-
-    UiGroupPanel document_group_;
-    UiDoc document_;
+    UiGroupPanel inputs_group_;
+    UiLineEdit line_edit_;
+    UiMultiEdit multi_edit_;
+    UiSliderEdit slider_edit_;
 
     UiGroupPanel data_group_;
+    UiList list_;
+    UiTree tree_;
+    UiTable table_;
+
+    UiGroupPanel navigation_group_;
     UiTab tab_;
     ParentCtrl tab_page_a_;
     ParentCtrl tab_page_b_;
-    UiTree tree_;
-    UiTable table_;
-    UiList list_;
     UiAccordion accordion_;
     ParentCtrl accordion_a_;
     ParentCtrl accordion_b_;
 
-    UiGroupPanel input_group_;
-    UiLineEdit line_edit_;
-    UiMultiEdit multi_edit_;
-    UiDropdown dropdown_;
-    UiCheckBox check_;
-    UiRadioButton radio_;
-    UiColorPicker color_;
-    UiSliderEdit slider_edit_;
-    UiBezierCurveField curve_;
+    UiGroupPanel feedback_group_;
+    UiLabel feedback_label_;
+    UiProgressBar feedback_progress_;
 
-    Array<UiGroupPanel> inventory_tiles_;
-    Array<UiLabel> inventory_titles_;
-    Array<Ctrl> inventory_controls_;
+    // Containers view: paired plain/populated surfaces make it possible to
+    // judge container chrome both alone and with representative content.
+    UiPanel container_plain_panel_;
+    UiLabel container_plain_label_;
+
+    UiPanel container_controls_panel_;
+    UiLabel container_controls_label_;
+    UiButton container_button_;
+    UiCheckBox container_check_;
+
+    UiGroupPanel container_plain_group_;
+    UiLabel container_group_label_;
+
+    UiGroupPanel container_edit_group_;
+    UiLineEdit container_edit_;
+    UiButton container_edit_button_;
+
+    UiPanel container_numeric_panel_;
+    UiLabel container_numeric_label_;
+    UiSlider container_slider_;
+    UiIntEdit container_int_;
+
+    UiGroupPanel container_choice_group_;
+    UiDropdown container_dropdown_;
+    UiToggle container_toggle_;
 };
 
 }
